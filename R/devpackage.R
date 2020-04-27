49d24bc645eda3aeb0fbc5cdad3146c301e50c22
demiperimetre <pierre.barbillon@agroparistech.fr>
2020-04-27 15:27:29 +0200

    Initial commit

---
+++ R/devpackage.R
@@ -0,0 +1,17 @@
+library(testthat)
+library(devtools)
+library(usethis)
+
+use_gpl3_license("Pierre")
+
+use_git()
+#use_github("git@github.com:Demiperimetre/PIPScreening.git")
+# en ligne de commande depuis les commandes de github
+
+use_readme_rmd() # creer la description
+
+use_lifecycle_badge("experimental")
+
+
+
+check()
